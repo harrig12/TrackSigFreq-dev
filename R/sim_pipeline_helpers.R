082e65a7834639249bee924ebb1d105c54e785b0
Cait <cait.harrigan@mail.utoronto.ca>
2019-03-19 23:27:44 -0400

    function comparison, rebuild

--- R/sim_pipeline_helpers.R
+++ R/sim_pipeline_helpers.R
@@ -14,13 +14,13 @@ kldiv_multinomials <- function(multinom1, multinom2) {
   return(apply(multinom1 * log(multinom1/multinom2),1,sum))
 }
 
-compare_simulation_results  <- function(simulation_list, 
+compare_simulation_results  <- function(simulation_list,
   ground_truth_dir, method_results_dir, res_file_name) {
 
   results_df <- c()
   gt_exposures_list <- list()
   estim_exposures_list <- list()
-  
+
   for (sim in simulation_list) {
     print(sprintf("Processing simulation %s ...", sim))
 
@@ -34,7 +34,7 @@ compare_simulation_results  <- function(simulation_list,
     gt_exposures <- read.delim(gt_exposures_file, header=T, stringsAsFactors=F)
     gt_pos = paste0(gt_exposures[,"chromosome"], "_", gt_exposures[,"start"])
     rownames(gt_exposures) <- gt_pos
-    
+
     estim_exposures_file = paste0(method_results_dir, "/", sim, "/", "sig_exposures_per_mut.txt")
 
     if (!file.exists(estim_exposures_file)) {
@@ -67,8 +67,8 @@ compare_simulation_results  <- function(simulation_list,
 
     sim_type <- gsub("Simulation_([A-z_]*)\\d\\d*_.*", "\\1", sim)
     depth <- as.integer(gsub(".*_depth(.*)", "\\1", sim))
-    
-    d <- data.frame(sim = sim, 
+
+    d <- data.frame(sim = sim,
       abs_diff_mean = mean(unlist(abs_diff), na.rm=TRUE),
       abs_diff_max = max(unlist(abs_diff), na.rm=TRUE),
       kl = mean(kldiv_multinomials(gt_exposures_d, estim_exposures_d_eps), na.rm=TRUE),
@@ -85,7 +85,7 @@ compare_simulation_results  <- function(simulation_list,
   results_df <- data.frame(results_df)
   rownames(results_df) <- results_df[,1]
   write.table(results_df, file = res_file_name, sep = "\t", row.names=F, quote=F)
-  
+
   return(list(results_df, gt_exposures_list, estim_exposures_list))
 }
 
@@ -95,12 +95,12 @@ plot_kl_results <- function(res, method, sim_type = "") {
     sim_type = paste0(".", sim_type)
   }
   pdf(paste0(method, "_simulation_results_KL_vs_max", sim_type, ".pdf"), width = 5, height=5)
-  plot(res$kl, res$abs_diff_max, main=paste0(method, " versus Truth"), 
+  plot(res$kl, res$abs_diff_max, main=paste0(method, " versus Truth"),
      xlab="KL", ylab="max abs diff", xlim=c(0, 0.5), ylim=c(0, 0.5))
   dev.off()
 
   pdf(paste0(method,"_simulation_results_KL_vs_mean", sim_type, ".pdf"), width = 5, height=5)
-  plot(res$kl, res$abs_diff_mean, main=paste0(method, " versus Truth"), 
+  plot(res$kl, res$abs_diff_mean, main=paste0(method, " versus Truth"),
      xlab="KL", ylab="mean abs diff", xlim=c(0, 0.5), ylim=c(0, 0.5))
   dev.off()
 
@@ -144,18 +144,18 @@ adjust_change_points_ <- function(change_points, mixtures, change_at_cp_threshol
 
 
 
-get_max_changes_one_tumor <- function(mixtures, signatures_to_compute, tumor_id, 
+get_max_changes_one_tumor <- function(mixtures, signatures_to_compute, tumor_id,
   change_at_cp_threshold = 0.06) {
   new_item <- toHorizontalMatrix(rep(0, length(signatures_to_compute)))
 
   colnames(new_item) <- signatures_to_compute
-  
+
   rownames(new_item) <- tumor_id
-  
+
   new_item_mean <- new_item_direction <- new_item
 
   new_item[,rownames(mixtures)] <- apply(mixtures, 1, max) - apply(mixtures, 1, min)
-  
+
   signature_big_change = names(which(apply(mixtures, 1, max) - apply(mixtures, 1, min) >= change_at_cp_threshold)) # 0.06
   signature_neg_change = names(which(apply(mixtures, 1, which.max) < apply(mixtures, 1, which.min)))
   signature_pos_change = names(which(apply(mixtures, 1, which.max) > apply(mixtures, 1, which.min)))
@@ -165,7 +165,7 @@ get_max_changes_one_tumor <- function(mixtures, signatures_to_compute, tumor_id,
 
   new_item_direction[,signature_pos_change] <- 1
   new_item_direction[,signature_neg_change] <- -1
-  
+
   new_item_mean[,rownames(mixtures)] <- apply(mixtures, 1, mean)
 
   return(list(new_item, new_item_direction, new_item_mean))
@@ -219,7 +219,7 @@ compare_changepoints  <- function(simulation_list, ground_truth_dir,
 
 
   results_df <- c()
-  
+
   for (sim in simulation_list) {
     print(sprintf("Processing simulation %s ...", sim))
     tracksig_cp_file = paste0(tracksig_results_dir, "/", sim, "/changepoints.txt")
@@ -242,12 +242,12 @@ compare_changepoints  <- function(simulation_list, ground_truth_dir,
 
     cp_tracksig_unadjusted = get_change_points(mixtures_tracksig)
 
-    adjusted_cp_tracksig = adjust_change_points_(which(cp_tracksig_unadjusted), mixtures_tracksig, 
+    adjusted_cp_tracksig = adjust_change_points_(which(cp_tracksig_unadjusted), mixtures_tracksig,
       change_at_cp_threshold = change_at_cp_threshold)
 
     adjusted_cp_tracksig <- remove_cp_with_same_direction(mixtures_tracksig, adjusted_cp_tracksig)
 
-    # read change-points from sciclone 
+    # read change-points from sciclone
     # If we ran sciclone using multiple clustering methods, we want to aggregate results from all of them
     cp_sciclone <- list()
     for (sciclone_results_dir in sciclone_results_dir_list) {
@@ -286,7 +286,7 @@ compare_changepoints  <- function(simulation_list, ground_truth_dir,
       n_gt_created_cp = 2
     }
 
-    d <- data.frame(sim = sim, 
+    d <- data.frame(sim = sim,
       cp_tracksig = cp_tracksig,
       cp_tracksig_adjusted = length(adjusted_cp_tracksig),
       n_gt_exposure_cp = n_gt_exposure_cp,
@@ -301,14 +301,14 @@ compare_changepoints  <- function(simulation_list, ground_truth_dir,
 
   rownames(results_df) <- results_df[,1]
   write.table(results_df, file = res_file_name, sep = "\t", row.names=F, quote=F)
-  
+
   return(results_df)
 }
 
 
 get_results_per_sim_type <- function(res, method_name) {
-  sim_types <- c("one_cluster", "two_clusters", 
-    "branching", "cna_plus", 
+  sim_types <- c("one_cluster", "two_clusters",
+    "branching", "cna_plus",
     "inf_site_viol_plus")
 
   print("Per sim type")
@@ -328,7 +328,7 @@ get_results_per_sim_type <- function(res, method_name) {
     idx <- grepl(paste0(d_type,"$"), sapply(res[,1],toString))
 
     if (sum(idx) > 1) {
-      plot_kl_results(res[idx,], "SciClone", sim_type = d_type)
+      plot_kl_results(res[idx,], method_name, sim_type = d_type)
     }
     print(paste0("Percentage of samples with KL larger than 0.05 in type ", d_type))
     print(mean(res[idx,]$kl > 0.05))
@@ -340,7 +340,7 @@ get_results_per_sim_type <- function(res, method_name) {
 
   for (sim_type in sim_types) {
     for (d_type in depth_types) {
-      idx <- grepl(sim_type, sapply(res[,1],toString)) 
+      idx <- grepl(sim_type, sapply(res[,1],toString))
       idx <- idx &  grepl(paste0(d_type,"$"), sapply(res[,1],toString))
 
       avg_kl_per_type[d_type, sim_type] <- mean(res[idx,]$kl)
